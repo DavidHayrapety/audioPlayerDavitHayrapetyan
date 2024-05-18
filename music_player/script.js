780fde829e288515832b4b1f4f03eb771dc7d089
DavidHyarpety <david.hayrapetyan.2006@gmail.com>
2024-05-18 13:22:23 +0400

    Final project

--- music_player/script.js
+++ music_player/script.js
@@ -21,45 +21,114 @@ let data = {
 
 let song = new Audio
 
-window.onload = function (){
+window.onload = function () {
     playSong()
 }
 
-let currentSong=0
-function playSong(){
+let currentSong = 0
+function playSong() {
     song.src = data.song[currentSong]
     let songTitle = document.getElementById("songTitle")
     songTitle.textContent = data.title[currentSong]
 
     let img = document.getElementsByClassName("row1")[0]
-    img.style.backgroundImage = "url(" +data.poster[currentSong] + ")"
-    
+    img.style.backgroundImage = "url(" + data.poster[currentSong] + ")"
+
     let main = document.getElementsByClassName("main")[0]
-    main.style.backgroundImage = "url(" +data.poster[currentSong] + ")"
-    
+    main.style.backgroundImage = "url(" + data.poster[currentSong] + ")"
+
     song.play()
 }
 
-function playOrPauseSong(){
+function playOrPauseSong() {
     let play = document.getElementById("play")
 
-    if(song.paused){
+    if (song.paused) {
         song.play()
         play.src = "images/pause.png"
-    }else {
-        
+    } else {
         song.pause()
         play.src = "images/play-button-arrowhead.png"
     }
 }
 
-song.addEventListener("timeupdate", function (){
+song.addEventListener("timeupdate", function () {
     let fill = document.getElementsByClassName("fill")[0]
-
-    let position = (song.currentTime/song.duration) * 99
-    console
+    let position = (song.currentTime / song.duration) * 99
     fill.style.marginLeft = position + "%"
 
+    convertTime(song.currentTime)
 
-
+    if (song.ended) {
+        next()
+    }
 })
+
+function convertTime(seconds) {
+    let currentTime = document.getElementsByClassName("currentTime")[0]
+
+    let min = Math.floor(seconds / 60)
+    let sec = Math.floor(seconds % 60)
+
+    min = (min < 10) ? "0" + min : min
+    sec = (sec < 10) ? "0" + sec : sec
+
+    let durMin = Math.floor(song.duration / 60)
+    let durSec = Math.floor(song.duration % 60)
+
+    durMin = (durMin < 10) ? "0" + durMin : durMin
+    durSec = (durSec < 10) ? "0" + durSec : durSec
+
+    currentTime.textContent = min + " : " + sec + " / " + durMin + " : " + durSec
+}
+
+function prev() {
+    currentSong--
+    play = document.getElementById("play")
+    if (currentSong < 0) {
+        currentSong = data.song.length - 1
+    }
+
+    playSong()
+    play.src = "images/pause.png"
+}
+
+function next() {
+    currentSong++
+    play = document.getElementById("play")
+    if (currentSong > data.song.length - 1) {
+        currentSong = 0
+    }
+
+    playSong()
+    play.src = "images/pause.png"
+}
+
+function mute() {
+    let mute = document.getElementById("mute")
+    if (song.muted) {
+        song.muted = false
+        mute.src = "images/volume.png"
+    } else {
+        song.muted = true
+        mute.src = "images/volume-mute.png"
+    }
+}
+
+function decrease() {
+    let mute = document.getElementById("mute")
+    song.volume -= 0.2
+    if (song.volume <= 0.1) {
+        mute.src = "images/volume-mute.png"
+        song.muted = true
+    }
+
+}
+function increase() {
+    let mute = document.getElementById("mute")
+    song.volume += 0.2
+    if (song.volume > 0.1) {
+        mute.src = "images/volume.png"
+        song.muted = false
+    }
+}
